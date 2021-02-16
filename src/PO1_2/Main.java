13080a14599e25dc521e41dd7ef1836da702e852
Bilyana Vatev <bilyanavatev@gmail.com>
2021-02-16 13:25:17 +0000

    homework problem

---
+++ src/PO1_2/Main.java
@@ -0,0 +1,21 @@
+package PO1_2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt).toArray();
+
+        System.out.println(sum(numbers, 0) );
+
+    }
+    private static int sum(int[] arr, int index){
+        if(index == arr.length -1){
+            return arr[index];
+        }
+        return arr[index] + sum(arr,index + 1);
+    }
+}
